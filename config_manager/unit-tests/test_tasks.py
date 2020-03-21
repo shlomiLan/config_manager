0a3c6b7b32ca93f347a3271e16dfd39ac63edd2a
shlomiLan <shlomilanton@gmail.com>
2020-03-21 16:00:25 +0200

    update test

--- config_manager/unit-tests/test_tasks.py
+++ config_manager/unit-tests/test_tasks.py
@@ -5,9 +5,10 @@ from config_manager.providers import Heroku
 class TestHeroku:
     def test_new(self):
         assert 1 == 2
-#     heroku_app_name = 'test1-shlomi'
-#     context = Context()
-#     heroku = Heroku(context, heroku_app_name)
+
+    heroku_app_name = 'test1-shlomi'
+    context = Context()
+    heroku = Heroku(context, heroku_app_name)
 #
 #     def test_reset_vars(self):
 #         env_vars = {
